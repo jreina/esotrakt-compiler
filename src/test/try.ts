b5802f16e5d01ec3f4a27a4f45e8b3c8596ccb67
Johnny Reina <johnny.r.reina@gmail.com>
2020-01-08 07:18:12 -0600

    Scripts and stuff

--- src/test/try.ts
+++
@@ -1,8 +0,0 @@
-import data from "./fixture";
-import { compile } from "../compile";
-import fs from 'fs';
-
-const tree = compile(data);
-fs.writeFileSync("blah.json", JSON.stringify(tree), {
-  encoding: "utf8"
-});
